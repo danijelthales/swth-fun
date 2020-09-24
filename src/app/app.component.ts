b9f2f33696cd107c1b1c269a262654027133f304
dgornjakovic <danijel.gornjakovic@prodyna.com>
2020-09-25 01:10:02 +0200

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,107 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'swth-fun';
+
+  assets: Asset[] = new Array();
+
+  swthAsset: Asset;
+  btcAsset: Asset;
+  ethAsset: Asset;
+  neoAsset: Asset;
+  lamboCollectibleAsset: Asset;
+  toyotaYarisAsset: Asset;
+  teslaAsset: Asset;
+
+  constructor(private httpClient: HttpClient) {
+    this.swthAsset = new Asset('0', 'swth');
+    this.assets.push(this.swthAsset);
+    this.swthAsset.value = 500000;
+    this.btcAsset = new Asset('0', 'btc');
+    this.assets.push(this.btcAsset);
+    this.ethAsset = new Asset('0', 'eth');
+    this.assets.push(this.ethAsset);
+    this.neoAsset = new Asset('0', 'neo');
+    this.assets.push(this.neoAsset);
+    this.lamboCollectibleAsset = new Asset('748', 'Lamborghini Aventador SVJ collectible');
+    this.assets.push(this.lamboCollectibleAsset);
+    this.toyotaYarisAsset = new Asset('15650', 'Toyota Yaris');
+    this.assets.push(this.toyotaYarisAsset);
+    this.teslaAsset = new Asset('39900', 'Tesla Cybertruck');
+    this.assets.push(this.teslaAsset);
+
+    this.getPrices();
+
+  }
+
+  public getPrices() {
+
+
+    this.httpClient.get('https://api.coingecko.com/api/v3/coins/switcheo')
+      .subscribe((data: object) => {
+        const result = JSON.parse(JSON.stringify(data));
+        this.swthAsset.price = result.market_data.current_price.usd;
+        this.httpClient.get('https://api.coingecko.com/api/v3/coins/bitcoin')
+          .subscribe((databtc: string) => {
+            const resultbtc = JSON.parse(JSON.stringify(databtc));
+            this.btcAsset.price = resultbtc.market_data.current_price.usd;
+            this.httpClient.get('https://api.coingecko.com/api/v3/coins/ethereum')
+              .subscribe((dataeth: string) => {
+                const resulteth = JSON.parse(JSON.stringify(dataeth));
+                this.ethAsset.price = resulteth.market_data.current_price.usd;
+                this.httpClient.get('https://api.coingecko.com/api/v3/coins/neo')
+                  .subscribe((dataneo: string) => {
+                    const resultneo = JSON.parse(JSON.stringify(dataneo));
+                    this.neoAsset.price = resultneo.market_data.current_price.usd;
+                    this.calculateValues(this.swthAsset, null);
+                  });
+              });
+          });
+      });
+
+
+  }
+
+
+  public calculateValues(asset: Asset, event) {
+    if (event == null) {
+      this.btcAsset.value = this.swthAsset.value * this.swthAsset.price / this.btcAsset.price;
+      this.ethAsset.value = this.swthAsset.value * this.swthAsset.price / this.ethAsset.price;
+      this.neoAsset.value = this.swthAsset.value * this.swthAsset.price / this.neoAsset.price;
+      this.lamboCollectibleAsset.value = this.swthAsset.value * this.swthAsset.price / this.lamboCollectibleAsset.price;
+      this.toyotaYarisAsset.value = this.swthAsset.value * this.swthAsset.price / this.toyotaYarisAsset.price;
+      this.teslaAsset.value = this.swthAsset.value * this.swthAsset.price / this.teslaAsset.price;
+    } else {
+      const val = event.srcElement.value * 1.0;
+      asset.value = val;
+      if (asset !== this.swthAsset) {
+        this.swthAsset.value = asset.value / asset.price / this.swthAsset.price;
+      }
+      this.btcAsset.value = this.swthAsset.value * this.swthAsset.price / this.btcAsset.price;
+      this.ethAsset.value = this.swthAsset.value * this.swthAsset.price / this.ethAsset.price;
+      this.neoAsset.value = this.swthAsset.value * this.swthAsset.price / this.neoAsset.price;
+      this.lamboCollectibleAsset.value = this.swthAsset.value * this.swthAsset.price / this.lamboCollectibleAsset.price;
+      this.toyotaYarisAsset.value = this.swthAsset.value * this.swthAsset.price / this.toyotaYarisAsset.price;
+      this.teslaAsset.value = this.swthAsset.value * this.swthAsset.price / this.teslaAsset.price;
+    }
+  }
+
+
+}
+
+class Asset {
+  price: number;
+  name: string;
+  value: number;
+
+  constructor(price, name) {
+    this.price = price;
+    this.name = name;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,95 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatInputModule} from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSortModule} from '@angular/material/sort';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
